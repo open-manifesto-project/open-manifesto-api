026fea321d5ef47e6e5803589a97a9a5bbe19ca5
pr3ssh <x@pr3ssh.net>
2019-10-28 13:47:36 +0100

    Change proposal_number for id on proposal serialization schema

--- manifesto/database/schemas/proposal.py
+++ manifesto/database/schemas/proposal.py
@@ -2,14 +2,13 @@ from flask_restplus import fields
 
 
 simple_serializer = {
-    'proposal_number': fields.Integer(description=''),
+    'id': fields.Integer(required=True, description='The manifesto identifier'),
     'body': fields.String(description=''),
     'topics': fields.List(fields.String(description='')),
 }
 
 serializer = {
     'id': fields.Integer(required=True, description='The manifesto identifier'),
-    'proposal_number': fields.Integer(description=''),
     'body': fields.String(description=''),
     'topics': fields.List(fields.String(description='')),
     'tags': fields.List(fields.String(description='')),
@@ -17,5 +16,6 @@ serializer = {
     'budget': fields.Boolean(description=''),
     'non_negotiable': fields.Boolean(description=''),
     'agents': fields.List(fields.String(description='')),
+    'proposal_number': fields.Integer(description=''),
     'id_manifesto': fields.Integer(description=''),
 }
